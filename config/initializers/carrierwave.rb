123002576374ba75bee9b3a6e3b48c65e3344918
El Flaco <redasterix89@gmail.com>
2014-07-15 12:41:47 -0500

    s3 keys env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAJWQBVWB4GDTUTGAA',
-      :aws_secret_access_key => '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',
+      :aws_access_key_id     => ENV['S3_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET'],
       :region                => 'us-west-2'
     }
   else
